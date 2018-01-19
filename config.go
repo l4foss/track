435d7122b5c4c55f5fa88d1d1fe044e073334773
l4 <l4.foss@gmail.com>
2018-01-20 01:12:41 +0700

    verbose output option and write to config file

--- config.go
+++ config.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"os"
+)
+
+var (
+	confSkel string = `{
+    "osu": %s,
+    "telegram": %s,
+    "broadcast": %d,
+    "interval": %d,
+	"limit": %d,
+    "trackdb": %s,
+	"verbose": 1,
+	"track": %s
+}`
 )
 
 type Config struct {
@@ -12,9 +27,13 @@ type Config struct {
 	Interval  int64    `json:"interval"`
 	Limit     int      `json:"limit"`
 	TrackDB   string   `json:"trackdb"`
+	Verbose   int      `json:"verbose"`
 	Users     []string `json:"track"`
 }
 
+/*
+* reads config from a json file into Config struct
+ */
 func readConfig(filename string) (Config, error) {
 	var config Config
 	data, err := ioutil.ReadFile(filename)
@@ -26,20 +45,39 @@ func readConfig(filename string) (Config, error) {
 	return config, nil
 }
 
+/*
+* generates sample config file
+ */
 func genConfig() error {
-	var conf string = `{
-    "osu": "OSU TOKEN",
-    "telegram": "TELEGRAM TOKEN",
-    "broadcast": BROADCAST_CHATID,
-    "interval": 60,
-	"limit": 50,
-    "trackdb": "data.db",
-	"track": ["Cookiezi", "Rafis"]
-}`
+	conf := fmt.Sprintf(confSkel, "[OSU TOKEN]",
+		"[TELEGRAM TOKEN]", 11111111, 60, 50, "track.db", 1, "[\"Cookiezi\"]")
 
 	err := ioutil.WriteFile("config.sample", []byte(conf), 0644)
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+/*
+* this function writes current config to config file
+* this could be useful when you change the database at runtime and want
+* user list to be synced with config file
+ */
+func writeConfig() error {
+	users, err := getUsers()
+	if err != nil {
+		return err
+	}
+
+	ulist := fmt.Sprintf("%v", users)
+	conf := fmt.Sprintf(confSkel, config.Osu, config.Telegram, config.Broadcast, config.Interval,
+		config.Limit, config.TrackDB, config.Verbose, ulist)
+
+	err = ioutil.WriteFile(os.Args[2], []byte(conf), 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
